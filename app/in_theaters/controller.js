7390c5902457e760d252979bcfd5023a17b96b32
AIBottle <aibottlec@163.com>
2016-11-25 17:21:41 +0800

    跨域请求的分析过程，不支持的代码

--- app/in_theaters/controller.js
+++ app/in_theaters/controller.js
@@ -16,9 +16,13 @@
 		//data.json数据来源http://api.douban.com/v2/movie/in_theaters
 		$scope.subjects= [];
 		$scope.message = '';
+		// 实现跨域请求
+		// 查看angular文档可分析到：angular中将所有的JSONP的callback都挂在了angular.callbacks这个对象上
+		// 我们必须给当前地址加上一个参数，callback=JSON_CALLBACK
+		var doubanApiAddress = "http://api.douban.com/v2/movie/in_theaters ";
 		$http({
-			method: 'GET',
-			url: '/movie/app/data.json'
+			method: 'jsonp',
+			url: doubanApiAddress+'?callback=JSON_CALLBACK',
 		}).then(function successCallback(response) {
 			if(response.status == 200){
 				// 此处代码是在异步请求完成后才执行（需要等一段时间）
