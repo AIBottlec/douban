1480a5b5c1f7e885ff2ff4bff42b25e4c1c5646c
AIBottle <aibottlec@163.com>
2016-11-25 10:48:12 +0800

    正在热映添加ajax请求，请求本地json数据

--- app/in_theaters/controller.js
+++ app/in_theaters/controller.js
@@ -1,223 +1,5 @@
 (function (angular) {
 	'use strict';
-	var data = [
-			{
-				"rating": {
-					"max": 10,
-					"average": 6.9,
-					"stars": "35",
-					"min": 0
-				},
-				"genres": [
-					"剧情",
-					"喜剧"
-				],
-				"title": "我不是潘金莲",
-				"casts": [
-					{
-						"alt": "https://movie.douban.com/celebrity/1050059/",
-						"avatars": {
-							"small": "http://img7.doubanio.com/img/celebrity/small/1691.jpg",
-							"large": "http://img7.doubanio.com/img/celebrity/large/1691.jpg",
-							"medium": "http://img7.doubanio.com/img/celebrity/medium/1691.jpg"
-						},
-						"name": "范冰冰",
-						"id": "1050059"
-					},
-					{
-						"alt": "https://movie.douban.com/celebrity/1274274/",
-						"avatars": {
-							"small": "http://img7.doubanio.com/img/celebrity/small/39703.jpg",
-							"large": "http://img7.doubanio.com/img/celebrity/large/39703.jpg",
-							"medium": "http://img7.doubanio.com/img/celebrity/medium/39703.jpg"
-						},
-						"name": "郭涛",
-						"id": "1274274"
-					},
-					{
-						"alt": "https://movie.douban.com/celebrity/1324043/",
-						"avatars": {
-							"small": "http://img7.doubanio.com/img/celebrity/small/58870.jpg",
-							"large": "http://img7.doubanio.com/img/celebrity/large/58870.jpg",
-							"medium": "http://img7.doubanio.com/img/celebrity/medium/58870.jpg"
-						},
-						"name": "大鹏",
-						"id": "1324043"
-					}
-				],
-				"collect_count": 53780,
-				"original_title": "我不是潘金莲",
-				"subtype": "movie",
-				"directors": [
-					{
-						"alt": "https://movie.douban.com/celebrity/1274255/",
-						"avatars": {
-							"small": "http://img3.doubanio.com/img/celebrity/small/45667.jpg",
-							"large": "http://img3.doubanio.com/img/celebrity/large/45667.jpg",
-							"medium": "http://img3.doubanio.com/img/celebrity/medium/45667.jpg"
-						},
-						"name": "冯小刚",
-						"id": "1274255"
-					}
-				],
-				"year": "2016",
-				"images": {
-					"small": "http://img7.doubanio.com/view/movie_poster_cover/ipst/public/p2378133884.jpg",
-					"large": "http://img7.doubanio.com/view/movie_poster_cover/lpst/public/p2378133884.jpg",
-					"medium": "http://img7.doubanio.com/view/movie_poster_cover/spst/public/p2378133884.jpg"
-				},
-				"alt": "https://movie.douban.com/subject/26630781/",
-				"id": "26630781"
-			},
-			{
-				"rating": {
-					"max": 10,
-					"average": 8.1,
-					"stars": "40",
-					"min": 0
-				},
-				"genres": [
-					"剧情",
-					"奇幻",
-					"冒险"
-				],
-				"title": "神奇动物在哪里",
-				"casts": [
-					{
-						"alt": "https://movie.douban.com/celebrity/1010503/",
-						"avatars": {
-							"small": "http://img3.doubanio.com/img/celebrity/small/9399.jpg",
-							"large": "http://img3.doubanio.com/img/celebrity/large/9399.jpg",
-							"medium": "http://img3.doubanio.com/img/celebrity/medium/9399.jpg"
-						},
-						"name": "埃迪·雷德梅恩",
-						"id": "1010503"
-					},
-					{
-						"alt": "https://movie.douban.com/celebrity/1316589/",
-						"avatars": {
-							"small": "http://img7.doubanio.com/img/celebrity/small/37005.jpg",
-							"large": "http://img7.doubanio.com/img/celebrity/large/37005.jpg",
-							"medium": "http://img7.doubanio.com/img/celebrity/medium/37005.jpg"
-						},
-						"name": "凯瑟琳·沃特斯顿",
-						"id": "1316589"
-					},
-					{
-						"alt": "https://movie.douban.com/celebrity/1027317/",
-						"avatars": {
-							"small": "http://img7.doubanio.com/img/celebrity/small/31252.jpg",
-							"large": "http://img7.doubanio.com/img/celebrity/large/31252.jpg",
-							"medium": "http://img7.doubanio.com/img/celebrity/medium/31252.jpg"
-						},
-						"name": "丹·福勒",
-						"id": "1027317"
-					}
-				],
-				"collect_count": 6932,
-				"original_title": "Fantastic Beasts and Where to Find Them",
-				"subtype": "movie",
-				"directors": [
-					{
-						"alt": "https://movie.douban.com/celebrity/1275147/",
-						"avatars": {
-							"small": "http://img7.doubanio.com/img/celebrity/small/9900.jpg",
-							"large": "http://img7.doubanio.com/img/celebrity/large/9900.jpg",
-							"medium": "http://img7.doubanio.com/img/celebrity/medium/9900.jpg"
-						},
-						"name": "大卫·叶茨",
-						"id": "1275147"
-					}
-				],
-				"year": "2016",
-				"images": {
-					"small": "http://img7.doubanio.com/view/movie_poster_cover/ipst/public/p2392444121.jpg",
-					"large": "http://img7.doubanio.com/view/movie_poster_cover/lpst/public/p2392444121.jpg",
-					"medium": "http://img7.doubanio.com/view/movie_poster_cover/spst/public/p2392444121.jpg"
-				},
-				"alt": "https://movie.douban.com/subject/25726614/",
-				"id": "25726614"
-			},
-			{
-				"rating": {
-					"max": 10,
-					"average": 8.2,
-					"stars": "40",
-					"min": 0
-				},
-				"genres": [
-					"喜剧",
-					"动画",
-					"奇幻"
-				],
-				"title": "海洋奇缘",
-				"casts": [
-					{
-						"alt": "https://movie.douban.com/celebrity/1364824/",
-						"avatars": {
-							"small": "http://img7.doubanio.com/img/celebrity/small/1479447814.43.jpg",
-							"large": "http://img7.doubanio.com/img/celebrity/large/1479447814.43.jpg",
-							"medium": "http://img7.doubanio.com/img/celebrity/medium/1479447814.43.jpg"
-						},
-						"name": "奥莉·卡瓦洛",
-						"id": "1364824"
-					},
-					{
-						"alt": "https://movie.douban.com/celebrity/1044707/",
-						"avatars": {
-							"small": "http://img3.doubanio.com/img/celebrity/small/196.jpg",
-							"large": "http://img3.doubanio.com/img/celebrity/large/196.jpg",
-							"medium": "http://img3.doubanio.com/img/celebrity/medium/196.jpg"
-						},
-						"name": "道恩·强森",
-						"id": "1044707"
-					},
-					{
-						"alt": "https://movie.douban.com/celebrity/1031855/",
-						"avatars": {
-							"small": "http://img7.doubanio.com/img/celebrity/small/13834.jpg",
-							"large": "http://img7.doubanio.com/img/celebrity/large/13834.jpg",
-							"medium": "http://img7.doubanio.com/img/celebrity/medium/13834.jpg"
-						},
-						"name": "艾伦·图代克",
-						"id": "1031855"
-					}
-				],
-				"collect_count": 294,
-				"original_title": "Moana",
-				"subtype": "movie",
-				"directors": [
-					{
-						"alt": "https://movie.douban.com/celebrity/1045252/",
-						"avatars": {
-							"small": "http://img7.doubanio.com/img/celebrity/small/48764.jpg",
-							"large": "http://img7.doubanio.com/img/celebrity/large/48764.jpg",
-							"medium": "http://img7.doubanio.com/img/celebrity/medium/48764.jpg"
-						},
-						"name": "罗恩·克莱蒙兹",
-						"id": "1045252"
-					},
-					{
-						"alt": "https://movie.douban.com/celebrity/1041456/",
-						"avatars": {
-							"small": "http://img3.doubanio.com/img/celebrity/small/1359182726.97.jpg",
-							"large": "http://img3.doubanio.com/img/celebrity/large/1359182726.97.jpg",
-							"medium": "http://img3.doubanio.com/img/celebrity/medium/1359182726.97.jpg"
-						},
-						"name": "约翰·马斯克",
-						"id": "1041456"
-					}
-				],
-				"year": "2016",
-				"images": {
-					"small": "http://img3.doubanio.com/view/movie_poster_cover/ipst/public/p2397960879.jpg",
-					"large": "http://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2397960879.jpg",
-					"medium": "http://img3.doubanio.com/view/movie_poster_cover/spst/public/p2397960879.jpg"
-				},
-				"alt": "https://movie.douban.com/subject/25793398/",
-				"id": "25793398"
-			}
-		];
 	//创建正在热映的模块
 	var module = angular.module('movie.in_theaters', ['ngRoute']);
 	//配置模块路由
@@ -227,9 +9,24 @@
 			controller: 'inTheatersController'
 		});
 	}]);
-	module.controller('inTheatersController', ['$scope',function ($scope) {
+
+	module.controller('inTheatersController', ['$scope','$http',function ($scope,$http) {
 		//控制器编写：1.设计暴露的数据；2.设计暴露的行为
 		// 引用豆瓣API https://developers.douban.com/wiki/?title=api_v2
-		$scope.subjects= data;
+		$scope.subjects= [];
+		$scope.message = '';
+		$http({
+			method: 'GET',
+			url: '/movie/app/data.json'
+		}).then(function successCallback(response) {
+			if(response.status == 200){
+				$scope.subjects = response.data.subjects;
+				console.log($scope.subjects);
+			}else{
+				$scope.message = '获取数据失败'+response.statusText;
+			}
+		}, function errorCallback(err) {
+			$scope.message = "请求失败"+err.statusText;
+		});
 	}]);
 })(angular);
