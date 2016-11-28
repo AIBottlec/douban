1f5c0fd60ec5230dc92ccbdf31e9761000eeb18d
AIBottle <aibottlec@163.com>
2016-11-28 16:49:59 +0800

    分页一-页面路由配置和内容呈现

--- app/in_theaters/controller.js
+++ app/in_theaters/controller.js
@@ -4,24 +4,31 @@
 	var module = angular.module('movie.in_theaters', ['ngRoute','movie.services.http']);
 	//配置模块路由
 	module.config(['$routeProvider', function ($routeProvider) {
-		$routeProvider.when('/in_theaters', {
+		$routeProvider.when('/in_theaters/:page', {
 			templateUrl: 'in_theaters/view.html',
 			controller: 'inTheatersController'
 		});
 	}]);
 
-	module.controller('inTheatersController', ['$scope','HttpService',function ($scope,HttpService) {
+	module.controller('inTheatersController', ['$scope','$routeParams','HttpService',function ($scope,$routeParams,HttpService) {
+		var count = 10;//每一页的数量
+		var page = parseInt($routeParams.page);//当前是第几页
+		var start = (page - 1) * count;//当前页数据从哪开始
 		//控制器编写：1.设计暴露的数据；2.设计暴露的行为
 		// 引用豆瓣API https://developers.douban.com/wiki/?title=api_v2
 		//data.json数据来源http://api.douban.com/v2/movie/in_theaters
 		$scope.subjects= [];
 		$scope.message = '';
 		$scope.totalCount = 0;
+		$scope.totalpages = 0;
 		$scope.loading = true;
-		HttpService.jsonp('http://api.douban.com/v2/movie/in_theaters',{},function (data) {
+		HttpService.jsonp('http://api.douban.com/v2/movie/in_theaters',
+			{start:start,count:count},
+			function (data) {
 			// console.log(data);
 			$scope.subjects = data.subjects;
 			$scope.totalCount = data.total;
+			$scope.totalpages = Math.ceil($scope.totalCount/count);
 			$scope.loading = false;
 			$scope.$apply('subjects');//apply的作用就是让指定的表达式重新同步
 		});
