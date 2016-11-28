dcd3fc701e261721b557f6a2b53d18da5cb39cce
AIBottle <aibottlec@163.com>
2016-11-28 15:05:02 +0800

    跨域请求完成

--- app/in_theaters/controller.js
+++ app/in_theaters/controller.js
@@ -1,7 +1,7 @@
 (function (angular) {
 	'use strict';
 	//创建正在热映的模块
-	var module = angular.module('movie.in_theaters', ['ngRoute']);
+	var module = angular.module('movie.in_theaters', ['ngRoute','movie.services.http']);
 	//配置模块路由
 	module.config(['$routeProvider', function ($routeProvider) {
 		$routeProvider.when('/in_theaters', {
@@ -10,29 +10,21 @@
 		});
 	}]);
 
-	module.controller('inTheatersController', ['$scope','$http',function ($scope,$http) {
+	module.controller('inTheatersController', ['$scope','HttpService',function ($scope,HttpService) {
 		//控制器编写：1.设计暴露的数据；2.设计暴露的行为
 		// 引用豆瓣API https://developers.douban.com/wiki/?title=api_v2
 		//data.json数据来源http://api.douban.com/v2/movie/in_theaters
 		$scope.subjects= [];
 		$scope.message = '';
-		// 实现跨域请求
+		$scope.totalCount = 0;
+		HttpService.jsonp('http://api.douban.com/v2/movie/in_theaters',{},function (data) {
+			// console.log(data);
+			$scope.subjects = data.subjects;
+			$scope.totalCount = data.total;
+			$scope.$apply('subjects');//apply的作用就是让指定的表达式重新同步
+		});
+		                   // 实现跨域请求
 		// 查看angular文档可分析到：angular中将所有的JSONP的callback都挂在了angular.callbacks这个对象上
 		// 我们必须给当前地址加上一个参数，callback=JSON_CALLBACK
-		var doubanApiAddress = "http://api.douban.com/v2/movie/in_theaters ";
-		$http({
-			method: 'jsonp',
-			url: doubanApiAddress+'?callback=JSON_CALLBACK',
-		}).then(function successCallback(response) {
-			if(response.status == 200){
-				// 此处代码是在异步请求完成后才执行（需要等一段时间）
-				$scope.subjects = response.data.subjects;
-				console.log($scope.subjects);
-			}else{
-				$scope.message = '获取数据失败'+response.statusText;
-			}
-		}, function errorCallback(err) {
-			$scope.message = "请求失败"+err.statusText;
-		});
 	}]);
 })(angular);
