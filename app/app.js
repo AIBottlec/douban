c39c60a0b5a71382de826ea7bb4699fef85e39ba
AIBottle <aibottlec@163.com>
2016-11-29 12:45:44 +0800

    导航状态切换一-给导航添加独立的控制器

--- app/app.js
+++ app/app.js
@@ -6,4 +6,16 @@ angular.module('movie', [
 	'movie.movie_list'
 ]).config(['$routeProvider', function ($routeProvider) {
 	$routeProvider.otherwise({redirectTo: '/in_theaters/1'});
+}]).controller('NavController',['$scope','$location',
+	function ($scope, $location) {
+		$scope.$location=$location;
+		$scope.$watch('$location.path()',function (now) {
+			if (now.startsWith('/in_theaters')) {
+				$scope.type = 'in_theaters';
+			} else if (now.startsWith('/coming_soon')) {
+				$scope.type = 'coming_soon';
+			} else if (now.startsWith('/top250')) {
+				$scope.type = 'top250';
+			}
+		})
 }]);
