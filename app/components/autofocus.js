e5c44d16d4c03d17ea7a480ccd7f0b2db7047d51
AIBottle <aibottlec@163.com>
2016-11-29 15:00:57 +0800

    auto-focus遗留问题解决

--- app/components/autofocus.js
+++ app/components/autofocus.js
@@ -4,21 +4,28 @@
 (function (angular) {
 	angular.module('movie.directives.auto_focus',[])
 		.directive('autoFocus',['$location',function ($location) {
-			var path = $location.path();
+			// var path = $location.path();
+			// console.log(path);
 			return{
 				restrict:'A',
 				link:function ($scope,iElm,iAttrs,controller) {
-					var aLink = iElm.children().attr("href");
-					var type = aLink.replace(/#(\/.+?)\/\d+/,'$1');
-					// console.log(path+'-----------'+type);
-					if (path.startsWith(type)){
-						iElm.addClass('active');
-					}
-					iElm.on('click',function () {
+					$scope.$location = $location;
+					$scope.$watch('$location.path()',function (now) {
+						// 当path发生变化时执行，now是变化后的值
+						var aLink = iElm.children().attr("href");
+						var type = aLink.replace(/#(\/.+?)\/\d+/,'$1');
+						// console.log(path+'-----------'+type);
+						if (now.startsWith(type)){
+							iElm.parent().children().removeClass('active');
+							iElm.addClass('active');
+						}
+					});
+
+					/*iElm.on('click',function () {
 						iElm.parent().children().removeClass('active');
 						iElm.addClass('active');
 						// window.iele = iElm;
-					})
+					})*/
 				}
 			}
 		}])
